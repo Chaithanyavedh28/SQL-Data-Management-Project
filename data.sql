aaa23de069ddb39cf54937255f2ea89f134c18d6
Chaithanyavedh28 <vedh663@gmail.com>
2025-11-20 23:18:58 +0530

    data.sql

---
+++ data.sql
@@ -0,0 +1,9 @@
+INSERT INTO Departments VALUES (1, 'HR');
+INSERT INTO Departments VALUES (2, 'Finance');
+INSERT INTO Departments VALUES (3, 'Engineering');
+
+INSERT INTO Employees VALUES (101, 'John Doe', 55000, 1, SYSDATE);
+INSERT INTO Employees VALUES (102, 'Aisha Khan', 72000, 3, SYSDATE - 50);
+INSERT INTO Employees VALUES (103, 'Rohit Sharma', 45000, 2, SYSDATE - 100);
+INSERT INTO Employees VALUES (104, 'Maria Gomez', 82000, 3, SYSDATE - 200);
+INSERT INTO Employees VALUES (105, 'Sameer Patel', 60000, 1, SYSDATE - 300);
